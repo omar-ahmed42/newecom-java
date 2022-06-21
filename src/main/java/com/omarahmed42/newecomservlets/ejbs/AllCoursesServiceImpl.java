3303e8b00bd192bb1ae90a5b279bd39f19925ed9
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-06-21 19:20:41 +0200

    Update AllCoursesServiceImpl.java

--- src/main/java/com/omarahmed42/newecomservlets/ejbs/AllCoursesServiceImpl.java
+++ src/main/java/com/omarahmed42/newecomservlets/ejbs/AllCoursesServiceImpl.java
@@ -7,23 +7,16 @@ import com.omarahmed42.newecomservlets.entities.CourseEntity;
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Singleton
 @LocalBean
-@Path("/bean/all/courses")
 public class AllCoursesServiceImpl implements AllCoursesService {
 
     @Inject
     private CourseDao courseDao;
 
     @Override
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<CourseEntity> getAllCourses() {
         return courseDao.findAllCoursesForAll();
     }
